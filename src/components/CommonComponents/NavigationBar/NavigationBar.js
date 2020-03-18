cf737fcd61ebece29daa00c77be479fc266215da
Baartooo <bartek.gajos19@gmail.com>
2020-03-18 22:40:24 +0100

    trying to shrink page on mobile to 100%

--- src/components/CommonComponents/NavigationBar/NavigationBar.js
+++ src/components/CommonComponents/NavigationBar/NavigationBar.js
@@ -1,36 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from "react-router-dom";
 import DropdownToggleButton from '../DropdownNav/DropdownToggleButton';
 import Logo from '../../../photos/logo.png';
 
 import './NavigationBar.css';
 
-class NavigationBar extends Component {
-    render() {
+const NavigationBar = (props) => {
+    return (
+        <header className='navbar'>
+            <nav className='navbar-navigation'>
+                <div className='navbar-logo'><NavLink exact to='/'><img src={Logo} className='navbar-logo-img' alt='barto-projects-logo'></img></NavLink></div>
+                <div className='navbar-navigation-items'>
+                    <ul>
+                        <li><NavLink exact to='/' activeClassName='active-navigation-link'>start.</NavLink></li>
+                        <li><NavLink exact to='/outdoor' activeClassName='active-navigation-link'>outdoor.</NavLink></li>
+                        <li><NavLink exact to='/urban' activeClassName='active-navigation-link'>urban.</NavLink></li>
+                        <li><NavLink exact to='/people' activeClassName='active-navigation-link'>people.</NavLink></li>
+                        <li><NavLink exact to='/analog' activeClassName='active-navigation-link'>analog.</NavLink></li>
+                        <li><NavLink exact to='/about' activeClassName='active-navigation-link'>about.</NavLink></li>
+                    </ul>
 
-        return (
-            <header className='navbar'>
-                <nav className='navbar-navigation'>
-                    <div className='navbar-logo'><NavLink exact to='/'><img src={Logo} className='navbar-logo-img' alt='barto-projects-logo'></img></NavLink></div>
-                    <div className='navbar-spacer' />
-                    <div className='navbar-navigation-items'>
-                        <ul>
-                            <li><NavLink exact to='/' activeClassName='active-navigation-link'>start.</NavLink></li>
-                            <li><NavLink exact to='/outdoor' activeClassName='active-navigation-link'>outdoor.</NavLink></li>
-                            <li><NavLink exact to='/urban' activeClassName='active-navigation-link'>urban.</NavLink></li>
-                            <li><NavLink exact to='/people' activeClassName='active-navigation-link'>people.</NavLink></li>
-                            <li><NavLink exact to='/analog' activeClassName='active-navigation-link'>analog.</NavLink></li>
-                            <li><NavLink exact to='/about' activeClassName='active-navigation-link'>about.</NavLink></li>
-                        </ul>
-
-                    </div>
-                    <div className='dropDownToggleButtonDiv'>
-                        <DropdownToggleButton click={this.props.dropdownClickHandler} />
-                    </div>
-                </nav>
-            </header>
-        );
-    }
+                </div>
+                <div className='dropDownToggleButtonDiv'>
+                    <DropdownToggleButton click={props.dropdownClickHandler} />
+                </div>
+            </nav>
+        </header>
+    );
 }
 
 export default NavigationBar;
