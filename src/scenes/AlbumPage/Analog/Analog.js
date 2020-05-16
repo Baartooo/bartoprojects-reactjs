0fbb8cdce24926f20466d3b429b1322836788390
Baartooo <bartek.gajos19@gmail.com>
2020-05-16 14:35:36 +0200

    added photos

--- src/scenes/AlbumPage/Analog/Analog.js
+++ src/scenes/AlbumPage/Analog/Analog.js
@@ -2,19 +2,38 @@ import React from 'react';
 
 import '../Album/Album.css';
 import Album from '../Album/Album';
+import Tunnel from '../../../assets/photos/analog/tunnel.jpg';
 import BmxPhoto from '../../../assets/photos/analog/bmx.jpg';
-import GraffittiPhoto from '../../../assets/photos/analog/graffitti.JPG';
 import ParkingPhoto from '../../../assets/photos/analog/parking.jpg';
 import DodgePhoto from '../../../assets/photos/analog/dodge.jpg';
-import RailsPhoto from '../../../assets/photos/analog/rails.jpg';
+import ShadowPeople from '../../../assets/photos/analog/shadowPeople.jpg';
+import Anna from '../../../assets/photos/analog/anna.jpg';
+import BushWhite from '../../../assets/photos/analog/bushWhite.jpg';
+import BushYellow from '../../../assets/photos/analog/bushYellow.jpg';
+import BushSunset from '../../../assets/photos/analog/bushSunset.jpg';
 import TramPhoto from '../../../assets/photos/analog/tram.jpg';
+import RailsPhoto from '../../../assets/photos/analog/rails.jpg';
+import Opel from '../../../assets/photos/analog/opel.jpg';
 
-const images = [BmxPhoto, GraffittiPhoto, ParkingPhoto, DodgePhoto, RailsPhoto, TramPhoto];
+const images = [
+    Tunnel,
+    BmxPhoto,
+    ParkingPhoto,
+    DodgePhoto,
+    ShadowPeople,
+    Anna,
+    BushWhite,
+    BushYellow,
+    BushSunset,
+    TramPhoto,
+    RailsPhoto,
+    Opel
+];
 
 const Outdoor = (props) => {
     return (
         <main className='album-wrapper'>
-            <Album images={images}/>
+            <Album images={images} />
 
 
         </main>
--- src/scenes/AlbumPage/Outdoor/Outdoor.js
+++ src/scenes/AlbumPage/Outdoor/Outdoor.js
@@ -7,17 +7,31 @@ import CastlePhoto from '../../../assets/photos/outdoor/castle.jpg';
 import MountainsView from '../../../assets/photos/outdoor/mountainsView.jpg';
 import Mountains from '../../../assets/photos/outdoor/mountains.jpg';
 import MountainsPath from '../../../assets/photos/outdoor/mountainsPath.jpg';
+import MorningFogGlow from '../../../assets/photos/outdoor/morningFogGlow.jpg';
+import MorningFogCentralTree from '../../../assets/photos/outdoor/morningFogCentralTree.jpg';
+import MorningFog from '../../../assets/photos/outdoor/morningFog.jpg';
+
 
 import Album from '../Album/Album';
 
-const images = [CampingPhoto, LonelyTreePhoto, CastlePhoto, MountainsView, Mountains, MountainsPath];
+const images = [
+    CampingPhoto,
+    LonelyTreePhoto,
+    CastlePhoto,
+    MountainsView,
+    Mountains,
+    MountainsPath,
+    MorningFogGlow,
+    MorningFogCentralTree,
+    MorningFog
+];
 
 const Outdoor = (props) => {
     return (
         <main className='album-wrapper'>
 
-            
-            <Album images={images}/>
+
+            <Album images={images} />
 
 
         </main>
