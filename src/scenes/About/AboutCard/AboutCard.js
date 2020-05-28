5f68732d0f5bb334c1eb371d1bdc29f87e667298
Baartooo <bartek.gajos19@gmail.com>
2020-05-28 22:30:43 +0200

    smooth about image loading

--- src/scenes/About/AboutCard/AboutCard.js
+++ src/scenes/About/AboutCard/AboutCard.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AboutCard.css';
 import Thumbnail from '../../../assets/img/aboutThumbnail.jpg'
 import { instagramLink, facebookLink } from '../../../constans/Links/links';
 
 
 const AboutCard = props => {
+    const [imageLoaded, setImageLoaded] = useState(false);
 
     return (
         <div className='about-card'>
-            <img className='about-card-thumbnail' src={Thumbnail} alt='thumbnail' />
+            <img
+                className={`about-card-thumbnail img-${imageLoaded ? 'visible' : 'hidden'}`}
+                src={Thumbnail} alt='thumbnail'
+                onLoad={()=>setImageLoaded(true)}
+
+            />
             <p className='about-card-title'>connect with me</p>
 
             <a className='about-card-link' href='mailto:bartek.gajos19@gmail.com'>bartek.gajos19@gmail.com</a>
