c979e0c0b855d1cd18b73bd4be163ae99179df34
Baartooo <bartek.gajos19@gmail.com>
2020-06-17 18:32:24 +0200

    moved descriptions to constans

---
+++ src/scenes/About/AboutDescription/AboutDescription.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import './AboutDescription.css';
+import { instagramLink, facebookLink } from '../../../constans/Links/links';
+import descriptionEN from '../../../constans/AboutPageDescription/en.json';
+import descriptionPL from '../../../constans/AboutPageDescription/pl.json';
+import PlFlag from '../../../assets/img/poland.svg';
+import EnFlag from '../../../assets/img/united-kingdom.svg';
+
+const lang = {
+  pl: 'pl',
+  en: 'en',
+};
+
+const AboutDescription = () => {
+  const [language, setLanguage] = useState(lang.pl)
+
+  let description;
+
+  switch (language) {
+    case lang.pl:
+      description = descriptionPL;
+      break;
+    case lang.en:
+      description = descriptionEN;
+      break;
+    default:
+      break;
+  }
+  const switchDescriptionLanguage = (language) => {
+    if (language === lang.pl || language === lang.en) {
+      setLanguage(language);
+    }
+  };
+
+  return (
+    <div className="about-description-wrapper">
+      <h1>Bartosz Gajos</h1>
+      <h2>{description.localization}</h2>
+      <div className="about-lang-picker">
+        <img
+          src={PlFlag}
+          className={`flag ${(language === lang.pl) ? 'active-flag' : ''}`}
+          onClick={() => switchDescriptionLanguage(lang.pl)}
+          alt="pl-flag"
+        />
+        <img
+          src={EnFlag}
+          className={`flag ${(language === lang.en) ? 'active-flag' : ''}`}
+          onClick={() => switchDescriptionLanguage(lang.en)}
+          alt="en-flag"
+        />
+      </div>
+
+      <p>
+        {description.firstParagraph}
+      </p>
+      <p>
+        {description.secondParagraph}
+      </p>
+      <p>
+        {description.thirdParagraph}
+        &nbsp;
+        <a className="about-description-link" href={instagramLink} target="blank">instagram</a>
+        &nbsp;
+        {description.and}
+        &nbsp;
+        <a className="about-description-link" href={facebookLink} target="blank">facebook.</a>
+         &nbsp;
+      </p>
+      <p>
+        {description.fourthParagraph}
+      </p>
+    </div>
+  );
+};
+
+export default AboutDescription;
